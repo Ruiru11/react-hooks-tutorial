0a42ff801494c6b2f613b63cc521d4810f7243ef
Erastus Ruiru <erastusruiru11@gmail.com>
2019-06-03 10:39:06 +0300

    Updates

---
+++ precache-manifest.1899bf3b138ae217b6bf41ed952be9cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c351018668cc18b69a1a445a194abd8a",
+    "url": "/react-hooks-tutorial/index.html"
+  },
+  {
+    "revision": "6d4d337adfb702c4b3ab",
+    "url": "/react-hooks-tutorial/static/css/main.87833bd2.chunk.css"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/react-hooks-tutorial/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "6d4d337adfb702c4b3ab",
+    "url": "/react-hooks-tutorial/static/js/main.9b204dc2.chunk.js"
+  },
+  {
+    "revision": "2f96228ec73b36a1c878",
+    "url": "/react-hooks-tutorial/static/js/runtime~main.3f225721.js"
+  },
+  {
+    "revision": "2ccadf259e3473d042d995018f109e1a",
+    "url": "/react-hooks-tutorial/static/media/wallpaper.2ccadf25.jpeg"
+  }
+]);
